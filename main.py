86adf83fefd9f4a13388907f8221e27670152329
the12 <the12arrow@gmail.com>
2022-04-06 02:09:25 +0300

    -refactoring

--- main.py
+++ main.py
@@ -48,46 +48,44 @@ signal.signal(signal.SIGINT, sigint_handler)
 class ToastMenu:
     def __init__(self):
         self.minutes = get_minutes()
-        self.move_mouse = get_mouse()
+        # self.move_mouse = get_mouse()
         self.example_sent = False
+        self.notif_sent = -1
 
     def notification_loop(self):
+        self.send_notification()
         sch.every(self.minutes).minutes.do(self.send_notification)
         while True:
-            if self.example_sent:
-                sch.run_pending()
-                time.sleep(1)
-            else:
-                self.send_notification()
-                self.example_sent = True
+            sch.run_pending()
+            time.sleep(1)
 
     def send_notification(self):
         notif = threading.Thread(target=self.notification, args=())
         notif.start()
+        self.notif_sent += 1
 
     def notification(self):
         print("Starting Thread")
         toast = Notification
         icon_path = os.getcwd() + "\\reminder.png"
-
         if self.example_sent:
-            toast = Notification(app_id="Reminder",
+            toast = Notification(app_id="Reminder nr: " + str(self.notif_sent),
                                  title="Send a new email",
-                                 msg="10 Minutes have passed!",
+                                 msg=str(self.minutes) + " minutes have passed!",
                                  icon=icon_path)
 
         if not self.example_sent:
             self.example_sent = True
-            toast = Notification(app_id="Reminder",
-                                 title="Windows Toast Test",
-                                 msg="This is how the notification will look",
+            toast = Notification(app_id="Reminder Example",
+                                 title="Windows Toast Example",
+                                 msg="This is how the notification will look in " + str(self.minutes) + " minutes.",
                                  icon=icon_path)
         toast.show()
         print("Sent notification at: " + str(time.strftime("%H:%M:%S", time.localtime())))
         del toast
         print("Exiting Thread")
 
-    def move_mouse(self):
+    def move_mouse_now(self):
         if self.move_mouse:
             pyautogui.moveTo(100, 100, duration=1)
             pyautogui.moveRel(0, 50, duration=1)
