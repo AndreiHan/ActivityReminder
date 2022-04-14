3e8c4f3304ebdcad9791ade9b5b4a9d52a35ba57
the12 <the12arrow@gmail.com>
2022-04-15 01:27:56 +0300

    -refactoring

--- config.py
+++ config.py
@@ -12,19 +12,20 @@ class Config:
 
     def create_new_config(self):
         if self.is_create_new_config():
-            self.get_user_config()
-            self.write_user_config()
+            self.import_config_from_user_input()
+            self.write_current_config_to_file()
             print("\nCreated new configuration")
+
             self.print_config()
-            self.write_user_config()
+            self.write_current_config_to_file()
         else:
-            self.get_json_config()
+            self.import_config_from_json()
 
     def is_create_new_config(self):
         ans_list_negative = ["y", "Y"]
         ans_list_positive = ["n", "N"]
 
-        if not bool(self.get_config()):
+        if not bool(self.get_config_from_file()):
             return True
 
         print("This is your old config\n")
@@ -39,27 +40,27 @@ class Config:
         return False
 
     def print_config(self):
-        dictionary = self.get_config()
+        dictionary = self.get_config_from_file()
         print(json.dumps(dictionary, sort_keys=False, indent=4))
 
-    def get_json_config(self):
-        dictionary = self.get_config()
+    def import_config_from_json(self):
+        dictionary = self.get_config_from_file()
         self.minutes = dictionary["minutes"]
         self.notif_nr = dictionary["notif_nr"]
         self.move_mouse = dictionary["move_mouse"]
 
-    def get_user_config(self):
-        self.minutes = self.get_minutes()
-        self.notif_nr = self.get_notification_number()
-        self.move_mouse = self.get_mouse()
+    def import_config_from_user_input(self):
+        self.minutes = self.get_user_input_minutes()
+        self.notif_nr = self.get_user_input_notif_nr()
+        self.move_mouse = self.get_user_input_mouse()
 
-    def write_user_config(self):
+    def write_current_config_to_file(self):
         dictionary = {"minutes": self.minutes, "notif_nr": self.notif_nr, "move_mouse": self.move_mouse}
 
         with open(self.location, "w+") as outfile:
             json.dump(dictionary, outfile)
 
-    def get_config(self):
+    def get_config_from_file(self):
 
         if os.path.isfile(self.location):
             try:
@@ -70,8 +71,7 @@ class Config:
                 data = {}
                 return data
 
-
-    def get_mouse(self):
+    def get_user_input_mouse(self):
         ans_list_positive = ["y", "Y"]
         ans_list_negative = ["n", "N"]
 
@@ -86,30 +86,31 @@ class Config:
         self.move_mouse = False
         return False
 
-    def get_minutes(self):
+    def get_user_input_minutes(self):
         message = "How often to receive a notification? [In minutes]: "
         minutes = 10
         try:
             minutes = int(input(message))
             if minutes <= 0:
                 print("0 or smaller cannot be used")
-                self.get_minutes()
+                self.get_user_input_minutes()
         except ValueError:
             print("Please only use numbers")
-            self.get_minutes()
+            self.get_user_input_minutes()
         self.minutes = minutes
         return minutes
 
-    def get_notification_number(self):
+    def get_user_input_notif_nr(self):
+        notif_nr = int()
         message = "How many notifications do you want to receive? "
         try:
             notif_nr = int(input(message))
             if notif_nr <= 0:
                 print("0 or smaller cannot be used")
-                self.get_notification_number()
+                self.get_user_input_notif_nr()
         except ValueError:
             print("Please only use numbers")
-            self.get_notification_number()
+            self.get_user_input_notif_nr()
 
         self.notif_nr = notif_nr
         return notif_nr
